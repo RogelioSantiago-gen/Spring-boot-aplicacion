d6ddafc91600f14adeb8ee46207732a314346c0e
rogelio martinez <rogelio.martinezsantiago1@gmail.com>
2021-08-04 13:41:30 -0700

    Commit inicial con eclipse

---
+++ aplicacion_innovation/src/main/java/com/innovation/demo/AplicacionInnovationApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionInnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionInnovationApplication.class, args);
+	}
+
+}
